702cb534ffcc2784f42b2a57cec13743eca33060
Mark <SoftBearFeet@users.noreply.github.com>
2016-02-15 18:38:23 -0900

    added info to have enemy/played displayed. *Player is not being displayed for some reason*

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -25,11 +27,35 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+
+ this.x = x;
+ this.y = y;
+ this.sprit = 'images/enemy-bug.png';
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.speed*dt
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {};
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemyOne = new Enemy(75,150);
+var allEnemies = [enemyOne];
+var playerOne = new Player(20,400);
+var player = playerOne;
 
 
 // This listens for key presses and sends the keys to your
